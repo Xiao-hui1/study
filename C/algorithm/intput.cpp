08d3f145dc8af961d38a802193eebd28200ec9c9
Xiao-hui1 <1714676404@qq.com>
2026-06-08 15:24:18 +0800

    取消上传的.exe文件

--- C/algorithm/intput.cpp
+++ C/algorithm/intput.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
-int main(){
-    float i=1.3;
-    cout<<i<<endl;
+int main()
+{
+    float i = 1.3;
+    cout << i << endl;
     /**
      *  oct 表示八进制
      *  hex 表示十六进制
@@ -19,8 +20,8 @@ int main(){
      *  scientific 以科学计数法的形式输出
      *  showpos 在输出正整数时加’+‘
      *  showpoint 在输出浮点数时加小数点
-     * 
-     * 
+     *
+     *
      * strchr() 查找字符
      * strrchr() 右侧查找字符
      * strstr() 查找字符串
@@ -28,10 +29,9 @@ int main(){
      * strupr() 转换为大写
      * **/
 
-
     int j;
-    cin>>j;
-    int *data = new int[j]; //动态数组
-    printf("%lf",i);
+    cin >> j;
+    int *data = new int[j]; // 动态数组
+    printf("%lf", i);
     return 0;
 }
